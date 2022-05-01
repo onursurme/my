c359c0234e5b614b208e79e7912e3f4c02cc3581
onursurme <onursurme@yahoo.com>
2022-05-01 23:57:41 +0300

    21_DoublyLinkedBase.py yazıldı

--- 21__DoublyLinkedBase.py
+++ 21__DoublyLinkedBase.py
@@ -5,4 +5,46 @@
 # __len__ , is_empty , _insert_between(self,e,predecessor,successor) returns the new node,
 # _delete_node(self,node) returns deleted element
 
+from tkinter import N
+
+
 class _DoublyLinkedBase():
+
+    class _Node():
+
+        __slots__ = '_element', '_prev', '_next'
+
+        def __init__(self, e, prev, next):
+            self._element = e
+            self._prev = prev
+            self._next = next
+
+    def __init__(self):
+        self.header = self._Node(None, None, None)
+        self.trailer = self._Node(None, None, None)
+        self.header._next = self.trailer
+        self.trailer._prev = self.header
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def _insert_between(self, e, predecessor, successor):
+        newnode = self._Node(e, predecessor, successor)
+        predecessor._next = newnode
+        successor._prev = newnode
+        self.size += 1
+        return newnode
+
+    def _delete_node(self, node):
+        p = node._prev
+        n = node._next
+        p._next = n
+        n._prev = p
+        self.size -= 1
+        element = node._element
+        node._prev = node._next = node._element = None
+        return element
