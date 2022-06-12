a05d8350481afd552138c7ef8bbe8125f5672e6f
onursurme <onursurme@yahoo.com>
2022-06-12 16:41:55 +0300

    fibo için functools lru_cache ile memoization yapıldı, global açıklandı

--- 7_fibo1.py
+++ 7_fibo1.py
@@ -33,20 +33,20 @@ def fibo1(n):   # iterative (yani not recursive)
     if n<2:
         return n
     a1,a2=0,1
-    for x in range(0,n-1):
+    for x in range(n-1): # range(0,n-1) ile range(n-1) aynı
         a1,a2=a2,a1+a2
     return a2
 
 # şu da olur :
 def fib(n):
     f1,f2=0,1
-    for x in range(0,n):
+    for x in range(n):
         f1,f2=f2,f1+f2
     return f1
 
 print(fibo1(4))
 print(fib(4))
-# ardışık 2 fibonacci sayısını list olarak tutan bir generator yaz
+# ardışık 2 fibonacci sayısını list olarak tutan ve her çağrıldığında sıradaki fibonacci sayısını veren bir generator yaz
 
 
 
@@ -131,9 +131,8 @@ def bad_fibo(n):   # çok kötü çünkü n 1 arttığında çağrılan fonksiyo
 
 
 
-def good_fibo(n):  # kitapta 1 yerine 2 değer dönen recursive bir Fibonacci fonksiyonu verilmiş
-                   # bad_fibo her seferinde 2 recursive call yaptığı için nonefficient.
-                   # burada ise tek çağrı hedeflenmiş
+def good_fibo(n):  # bad_fibo her seferinde 2 recursive call yaptığı için nonefficient.
+                   # burada ise tek çağrı yapıldığı için daha efficient
     if n<2:
         return [n,0]
     s=good_fibo(n-1)
@@ -160,37 +159,38 @@ input()
 
 
 
+lasty=1
 
 def fibo4(n):   # n 0 veya 1 değeriyle başlayıp giderek artan sırada çağrılırsa hiç miss alınmıyor
                 # ama bunu yapmadan direk fibo4(10) gibi bir çağrı yapılırsa miss'ler yaşanıyor
                 # recursive (fibo3'ten daha efficient). burada amacım daha önce hesaplanan değerlerin yeniden hesaplanmamasını
                 # sadece "daha önce hesaplanmamış olan" değerlerin  hesaplanmasını sağlamak
                 # bu yaklaşımın neresi zayıf/geliştirilebilir : baştan 100 uzunlukta array açıldı, fazla gelirse, yetmezse?
-    global last # bunu yazmazsak aşağıda last'ı kullanmak istediğimizde last'ı bu fonksiyona ait lokal bir değişken zanneder ve
+    global lasty # bunu yazmazsak aşağıda last'ı kullanmak istediğimizde last'ı bu fonksiyona ait lokal bir değişken zanneder ve
                 # değer atamadan kullanıyorsun diye hata verir
-    if last>=n:
+                # bknz globaldegisken_python.py isimli programım
+    if lasty>=n:
         print("hit : ", n)
         return fibo4data[n]
-    if last<n-2:
-        print("miss :", n-2," last=",last)
+    if lasty<n-2:
+        print("miss :", n-2," last=",lasty)
         fibo4data[n-2]=fibo4(n-2)
-        last=n-2   # daha küçük bir terim hesaplanmadan daha büyük terim hesaplanamaz
+        lasty=n-2   # daha küçük bir terim hesaplanmadan daha büyük terim hesaplanamaz
                    # yani n-2'yi hesaplıyorsak o ana kadar hesapladğımız en büyük terim n-2. terim demektir
 
-    if last<n-1:
-        print("miss :",n-1," last=",last)
+    if lasty<n-1:
+        print("miss :",n-1," last=",lasty)
         fibo4data[n-1]=fibo4(n-1)
-        last=n-1
+        lasty=n-1
     fibo4data[n]=fibo4data[n-2]+fibo4data[n-1]
-    last=n
-    print(n,"=",n-2,"(hit) +",n-1," (hit)  last=",last)
+    lasty=n
+    print(n,"=",n-2,"(hit) +",n-1," (hit)  last=",lasty)
     return fibo4data[n]
 
 
 fibo4data=[0]*100
 fibo4data[0]=0
 fibo4data[1]=1
-last=1
 print("sonuç = ",fibo4(13))
 print("sonuç = ",fibo4(16))
 print("sonuç = ",fibo4(8))
@@ -199,8 +199,8 @@ print(fibo4data)
 print("----------------------------")
 for x in range(10):
     print(x," : ",fibo4(x))
-print("last",last)
-print(fibo4data[:last+1])
+print("last",lasty)
+print(fibo4data[:lasty+1])
 
 # for x in range(0,10):
 #     print(x+1," : ",fibo4(x))
@@ -209,3 +209,39 @@ print(fibo4data[:last+1])
 # f2=fibo2()
 # for x in range(0,10):
 #     print(x+1," : ",next(f2))
+
+# bu yaptığım memoization'ın Python'ın bir özelliğini kullanarak yapılması : 
+
+from functools import lru_cache
+
+@lru_cache()
+def fibonacci(n):
+    print('Enter', n)
+    if n==0:
+        x = 0
+    elif n==1:
+        x = 1
+    else:
+        x = fibonacci(n-1) + fibonacci(n-2)
+    print('Exit', n)
+    return x
+
+print(fibonacci(4))
+
+# diğer bir memoization (benim fibo4 ün daha basit hali)
+
+cache = dict()
+
+def fibonacci2(n):
+    if n in cache:
+        return cache[n]
+    if n==0:
+        x = 0
+    elif n==1:
+        x = 1
+    else:
+        x = fibonacci(n-1) + fibonacci(n-2)
+        cache[n] = x
+        return x
+
+print(fibonacci2(4))
